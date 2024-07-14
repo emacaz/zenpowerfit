5e7718f072dbb77fb8c3a139d3aa4a9acf5f619f
Emanuel Castillo <ecastillo76@misena.edu.co>
2024-07-14 12:59:35 +0200

    Design get guide by email or paid

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-document.getElementById("play_btn_id").addEventListener("click", function () {
-  let video = document.getElementById("myVideo");
-  if (video.paused || video.ended) {
-    video.play();
-    document.getElementById("play_btn_id").style.display = "none";
+// document.getElementById("play_btn_id").addEventListener("click", function () {
+//   let video = document.getElementById("myVideo");
+//   if (video.paused || video.ended) {
+//     video.play();
+//     document.getElementById("play_btn_id").style.display = "none";
     
-  } else {
-    video.pause();
-  }
-});
+//   } else {
+//     video.pause();
+//   }
+// });
 
 function importHotmart() {
   var imported = document.createElement("script");
