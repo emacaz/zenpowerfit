7e4d76007e1feb398648a37c56c956eba6a02c9a
Emanuel Castillo <ecastillo76@misena.edu.co>
2024-07-21 18:17:30 +0200

    Can send emails from front to back

--- script.js
+++ script.js
@@ -10,22 +10,43 @@ function importHotmart() {
 }
 importHotmart();
 
-// document.getElementById("form_id").addEventListener("submit", (e) => {
-//   e.preventDefault();
+document.getElementById("form_id").addEventListener("submit", (e) => {
+  e.preventDefault();
 
-//   const responseMessage = document.getElementById("responseMessage");
-//   const email = document.getElementById("email_id").value;
-//   const submitBtn = document.getElementById("submitBtn");
-//   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
+  const API = 'https://3ewf2lnjmk.execute-api.sa-east-1.amazonaws.com/confirm-lead';
+  const responseMessage = document.getElementById("responseMessage");
+  const email = document.getElementById("email_id").value.toLowerCase().replace(/\s/g, "");
+  const submitBtn = document.getElementById("submitBtn");
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   
-//   document.querySelector("input[name='email']").disabled = true;
-//   submitBtn.textContent = "Enviando...";
+  document.querySelector("input[name='email']").disabled = true;
+  submitBtn.textContent = "Enviando...";
 
-//   if (!emailRegex.test(email)) {
-//     responseMessage.textContent = "Ingresa un correo electrónico válido.";
-//     submitBtn.textContent = "Enviar";
-//     document.querySelector("input[name='email']").disabled = false;
-//     return;
-//   }
-// })
+  if (!emailRegex.test(email)) {
+    responseMessage.textContent = "Ingresa un correo electrónico válido.";
+    submitBtn.textContent = "Enviar";
+    document.querySelector("input[name='email']").disabled = false;
+    return;
+  }
+
+  fetch(API, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email: email }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      submitBtn.textContent = "Enviar";
+      document.querySelector("input[name='email']").disabled = false;
+      document.querySelector("input[name='email']").value = "";
+      responseMessage.textContent = "";
+      // document.getElementById("confirmationModal").style.display = "block";
+      // grecaptcha.reset();
+    })
+    .catch((error) => {
+      console.error("Error", error);
+    });
+})
